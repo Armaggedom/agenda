acbb70c48a0d9dc793c5093f5d115ff87a3b339f
Miyu <doctor12221@gmail.com>
2023-02-26 21:28:12 -0300

    V:1.2.0 microCorrection

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ httpServer.listen(process.env.PORT || 5000, ()=>{
 	console.log('\x1b[33m[Cardial Info]\x1b[0m : Server Uploaded')
 	console.log('\x1b[33m[Cardial Info]\x1b[0m : API V:'+packageUrl.version)
 	console.log('\x1b[33m[Cardial Info]\x1b[0m : Desc: '+packageUrl.description)
-	console.log('\x1b[33m[Cardial Info]\x1b[0m : rodando localmente pelo IP: '+process.env.PORT+ 'ou Porta 5000')
+	console.log('\x1b[33m[Cardial Info]\x1b[0m : rodando localmente pelo IP: '+process.env.PORT+ ' ou Porta 5000')
 	console.log('------------------------------------------')
 })
