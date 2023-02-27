5cb70a4334e2e0e95c11fc5afd2e2cf325abb740
Miyu <doctor12221@gmail.com>
2023-02-26 21:23:47 -0300

    V:1.2.0

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+const packageUrl=require('./package.json')
 const express=require('express')
+//to server
 const { createServer }=require('http')
 const path=require('path')
 const app=express()
@@ -7,4 +10,8 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 httpServer.listen(process.env.PORT || 5000, ()=>{
 	console.clear()
 	console.log('\x1b[33m[Cardial Info]\x1b[0m : Server Uploaded')
+	console.log('\x1b[33m[Cardial Info]\x1b[0m : API V:'+packageUrl.version)
+	console.log('\x1b[33m[Cardial Info]\x1b[0m : Desc: '+packageUrl.description)
+	console.log('\x1b[33m[Cardial Info]\x1b[0m : rodando localmente pelo IP: '+process.env.PORT+ 'ou Porta 5000')
+	console.log('------------------------------------------')
 })
